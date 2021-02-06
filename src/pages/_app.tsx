dc234f542ec1f9efaf25938417f7cadec5ee4692
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-02-06 01:50:23 -0300

    creating src folder structure, styles  and babel configure

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import GlobalStyles from "../styles/GlobalStyles";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+
