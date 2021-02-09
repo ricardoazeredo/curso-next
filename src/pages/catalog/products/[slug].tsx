3ee33500ac2120a5c0524e5d736dae0c597cb921
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-02-09 17:24:36 -0300

    Adding lazy load components and dynamic imports

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,7 +1,28 @@
 import { useRouter } from 'next/router';
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+
+const AddToCartModal = dynamic(
+  () => import('../../../components/AddToCartModal'),
+  { loading: () => <p>Loading...</p>}
+)
+//import AddToCartModal from '../../../components/AddToCartModal';
 
 export default function Product() {
   const router = useRouter();
+  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
 
-  return <h1>{router.query.slug}</h1>
+  function handleAddToCart() {
+    setIsAddToCartModalVisible(true);
+  }
+
+  return (
+    <div>
+      <h1>{router.query.slug}</h1>
+
+      <button onClick={handleAddToCart}>Add to cart</button>
+
+      { isAddToCartModalVisible && <AddToCartModal />}
+    </div>  
+  )
 }
